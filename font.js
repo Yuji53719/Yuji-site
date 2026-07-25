0fec61a5868b844e5d7696d4ef6c2b56b6c61433
甲魚 <yuji53728@icloud.com>
2026-07-25 20:47:54 +0800

    新增評論、連載頁與登入修復

--- font.js
+++
@@ -1,5 +0,0 @@
-export const siteFont = 'PMingLiU, "新細明體", MingLiU, serif';
-
-const style = document.createElement("style");
-style.textContent = `html,body,button,input,textarea,select,[contenteditable="true"]{font-family:${siteFont}!important}`;
-document.head.appendChild(style);
